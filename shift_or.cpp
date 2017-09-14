2e33a196a7c0a4c866be32276fb51db8e60e4b5d
Lucas Vinicius da Costa Santana <lvcs@ug5c29.windows.cin.ufpe.br>
2017-09-14 16:55:00 -0300

    editando shift or e adicionado string dist

--- shift_or.cpp
+++ shift_or.cpp
@@ -6,7 +6,7 @@
 
 class MyBitSet {
 public:
-  MyBitSet(){}
+  MyBitSet() {}
   MyBitSet(int length, bool start = true) {
     int ceil = (length + SIZE - 1) / SIZE;
     this->length = ceil;
@@ -18,15 +18,50 @@ public:
   }
 
   int operator[](int pos) {
-    int id = pos / SIZE, in = pos % SIZE;
-    return (bits[id] >> in)&1;
+    int bucket = pos / SIZE, id = pos % SIZE;
+    return (bits[bucket] >> id)&1;
+  }
+
+  void reset(int pos) {
+    int bucket = pos / SIZE, id = pos % SIZE;
+    bits[bucket] &= ~(UM << id);
   }
   
+  void reset(int pos) {
+    int bucket = pos / SIZE, id = pos % SIZE;
+    bits[bucket] |= (UM << id);
+  }
+
+  MyBitSet operator<<=(int shift) {
+    for (int i = length - 1; i > -1; --i) {
+      __uint128_t val = bits[i];
+      int pos = (i + 1) * SIZE - 1 + shift;
+      bits[i] = 0;
+      set_mask(pos, val);
+    }
+    return *this;
+  }
+
   MyBitSet operator<<(int shift) {
     MyBitSet ans(SIZE * ceil, false);
     for (int i = length - 1; i > -1; --i) {
-
+      __uint128_t val = bits[i];
+      int pos = (i + 1) * SIZE - 1 + shift;
+      ans.set_mask(pos, val);
     }
+    return ans;
+  }
+
+  __uint128_t& get_bucket(int buck) {
+    return bits[buck];
+  }
+
+  MyBitSet operator|(const MyBitSet &bit_array) {
+    MyBitSet ans = bit_array;
+    for (int i = 0; i < length; i++) {
+      ans.get_bucket(i) |= bits[i];
+    }
+    return ans;
   }
 private:
   const int SIZE = sizeof(__uint128_t) * 8;
@@ -35,14 +70,26 @@ private:
   std::vector<__uint128_t> bits;
 
   void set_mask(int pos, __uint128_t mask) {
-    int id = pos / SIZE, in = pos % SIZE;
-    bits[id] |= (mask >> (SIZE - 1 - in));
-    if (id - 1 >= 0 &&) {
-      bits[id - 1] |= (mask << in)
+    int bucket = pos / SIZE, id = pos % SIZE;
+    if (bucket < length) {
+      bits[bucket] |= (mask >> (SIZE - 1 - id));
+    }
+    if (bucket - 1 >= 0 && id + 1 < SIZE) {
+      bits[bucket - 1] |= (mask << (id + 1))
     }
   }
-
 };
+
+std::vector<MyBitSet> char_mask(std::string P, int sigmaSize) {
+  std::vector<MyBitSet> ans(sigmaSize, MyBitSet(P.size()));
+  MyBitSet stamp(P.size());
+  stamp.reset(0);
+  for (int i = 0; i < P.size(); i++) {
+    ans[P[i]] &= stamp;
+    stamp <<= 1;
+    stamp.reset(0);
+  }
+}
 
 void printVector(std::vector<int> v){
   std::cout << "[";
