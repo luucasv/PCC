6bddbcbef9c56e4f3ff934e4164dc70126480f19
Lucas Vinicius da Costa Santana <lvcs@ug5c30.windows.cin.ufpe.br>
2017-09-21 17:00:02 -0300

    ukkonen

--- ukkonen.cpp
+++ ukkonen.cpp
@@ -3,6 +3,62 @@
 #include <vector>
 #include <algorithm>
 
+
+class HashTree {
+public:
+  HashTree() {
+    root = new Node;
+    next_id = 0;
+  }
+
+  ~HashTree() {
+    delete root;
+  }
+
+  int get_id (const std::vector<int> &v) {
+    Node *cur = root;
+    for (int i = 1; i < v.size(); i++) {
+      int delta = v[i] - v[i-1] + 1;
+      if (cur[delta] == NULL) {
+        cur[delta] = new Node();
+      }
+      cur = cur[delta];
+    }
+    if (cur.id() == -1) {
+      cur.id() = next_id++;
+    }
+    return cur.id();
+  }
+private:
+  class Node {
+  public:
+    Node() {
+      children[0] = children[1] = children[2] = NULL;
+      id = -1;
+    }
+
+    ~Node() {
+      for (int i = 0; i < 3; i++) {
+        if (children[i] != NULL)
+          delete children[i];
+      }
+    }
+
+    Node* operator[](int i) {
+      return children[i];
+    }
+
+    int& id() {
+      return id;
+    }
+  private:
+    Node *children[3];
+    int id;
+  };
+  int next_id;
+  Node *root;
+}
+
 int edit_distance(const std::string &x, const std::string &y) {
   int n = x.size(), m = y.size();
   std::vector<std::vector<int>> memo(2, std::vector<int>(m+1));
